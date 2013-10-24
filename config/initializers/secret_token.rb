595f630faed02cd69808d86ffdad3720aff75877
root <root@progweb>
2013-10-24 22:44:31 +0200

    Depôt initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '2bd2444d32ce9fd984ca0341cf76a7b4ed4e052f85fc089d66df669b98ad27714abdcb2a7ae5aea88ccbc077714b1ee17c7cd0fc964189ab69ec5213e299881e'
